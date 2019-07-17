ae917f0bc959c1793ef597d7e90f3654340eab87
CindyChenx <chen_cx0128@hotmail.com>
2019-07-17 11:31:30 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+
+let index = 0;
+
 function init() {
   // your code here
+
+  document.body.addEventListener('keydown', function(e){
+    const key = e.key //e.which => value of the pressed keyboard
+
+    if (key === codes[index]){
+      index++;
+
+      if (index === codes.length){
+        alert('Congrats!!!!');
+        index = 0;
+      } 
+    } else {
+        index = 0;
+    }  
+  });
 }
+
